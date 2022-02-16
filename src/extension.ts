6dde81a1c250952199e70e3dd97c6e5e89ee24f1
Sander Ronde <awsdfgvhbjn@gmail.com>
2022-02-16 23:34:51 +0100

    more dev env things

--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,18 @@
 import * as vscode from 'vscode';
 
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: vscode.ExtensionContext): void {
 	console.log(
 		'Congratulations, your extension "phpstan-vscode" is now active!'
 	);
-	let disposable = vscode.commands.registerCommand(
+	const disposable = vscode.commands.registerCommand(
 		'phpstan-vscode.helloWorld',
-		() => {
-			vscode.window.showInformationMessage('Hello World from phpstan!');
+		async () => {
+			await vscode.window.showInformationMessage(
+				'Hello World from phpstan!'
+			);
 		}
 	);
 	context.subscriptions.push(disposable);
 }
 
-export function deactivate() {}
+export function deactivate(): void {}
