ea5011ded7e5f4758553c97914d68efc296b1465
Sander Ronde <awsdfgvhbjn@gmail.com>
2022-07-23 03:23:55 +0200

    ensure loading spinner is not interrupted by text refreshes

--- client/src/lib/statusBar.ts
+++ client/src/lib/statusBar.ts
@@ -3,17 +3,14 @@ import type { LanguageClient } from 'vscode-languageclient/node';
 import { statusBarNotification } from './notificationChannels';
 import { OperationStatus } from '../../../shared/statusBar';
 import { assertUnreachable } from '../../../shared/util';
+import { log, STATUS_BAR_PREFIX } from './log';
 import { getConfiguration } from './config';
 import type { Disposable } from 'vscode';
 import * as vscode from 'vscode';
-import { log } from './log';
 
 export class StatusBar implements Disposable {
 	private readonly _opTracker: OperationTracker;
-	private readonly _statusBar = vscode.window.createStatusBarItem(
-		vscode.StatusBarAlignment.Right,
-		1
-	);
+	private readonly _textManager = new TextManager();
 	private _hideTimeout: NodeJS.Timer | undefined;
 
 	public constructor(
@@ -46,8 +43,10 @@ export class StatusBar implements Disposable {
 		if (this._hideTimeout) {
 			clearInterval(this._hideTimeout);
 		}
-		this._statusBar.text = 'PHPStan checking.. $(loading~spin)';
-		this._statusBar.show();
+		this._textManager.setText(
+			`PHPStan checking.. ${TextManager.LOADING_SPIN}`
+		);
+		this._textManager.show();
 	}
 
 	private _hideStatusBar(lastResult: OperationStatus): void {
@@ -57,18 +56,18 @@ export class StatusBar implements Disposable {
 			lastResult
 		);
 		if (lastResult === OperationStatus.KILLED) {
-			this._statusBar.text = 'PHPStan process killed (timeout)';
+			this._textManager.setText('PHPStan process killed (timeout)');
 		} else if (lastResult === OperationStatus.SUCCESS) {
-			this._statusBar.text = 'PHPStan checking done';
+			this._textManager.setText('PHPStan checking done');
 		} else if (lastResult === OperationStatus.ERROR) {
-			this._statusBar.text = 'PHPStan checking errored (see log)';
+			this._textManager.setText('PHPStan checking errored (see log)');
 		} else if (lastResult !== OperationStatus.CANCELED) {
 			assertUnreachable(lastResult);
 		}
-		this._statusBar.text = 'PHPStan checking done';
+		this._textManager.setText('PHPStan checking done');
 		this._hideTimeout = setTimeout(
 			() => {
-				this._statusBar.hide();
+				this._textManager.hide();
 			},
 			lastResult === OperationStatus.ERROR ? 2000 : 500
 		);
@@ -79,8 +78,10 @@ export class StatusBar implements Disposable {
 	}
 
 	private operationProgress(progress: StatusBarProgress): void {
-		this._statusBar.text = `PHPStan checking project ${progress.done}/${progress.total} - ${progress.percentage}% $(loading~spin)`;
-		this._statusBar.show();
+		this._textManager.setText(
+			`PHPStan checking project ${progress.done}/${progress.total} - ${progress.percentage}% ${TextManager.LOADING_SPIN}`
+		);
+		this._textManager.show();
 	}
 
 	private finishOperation(
@@ -92,7 +93,7 @@ export class StatusBar implements Disposable {
 
 	public dispose(): void {
 		this._opTracker.dispose();
-		this._statusBar.dispose();
+		this._textManager.dispose();
 	}
 }
 
@@ -144,5 +145,74 @@ class Resolvable {
 	public complete(result: OperationStatus): void {
 		this.done = true;
 		this.result = result;
+	}
+}
+
+/**
+ * When updating the content of the statusbar, the spinning icon will start
+ * from its initial position. That's kind of ugly so we wait with showing
+ * the new text until it's restarting the animation.
+ */
+class TextManager implements Disposable {
+	public static readonly LOADING_SPIN = '$(loading~spin)';
+	private readonly _statusBar = vscode.window.createStatusBarItem(
+		vscode.StatusBarAlignment.Right,
+		1
+	);
+	private _currentStatusBarText: string = '';
+	private _pendingStatusBarText: string = '';
+	private _statusBarInterval: NodeJS.Timeout | null = null;
+
+	public constructor() {}
+
+	private _pushStatusBarText(): void {
+		if (this._statusBar.text === this._pendingStatusBarText) {
+			return;
+		}
+		this._statusBar.text = this._pendingStatusBarText;
+		this._currentStatusBarText = this._pendingStatusBarText;
+	}
+
+	public setText(text: string): void {
+		if (this._currentStatusBarText === text) {
+			return;
+		}
+		if (!text.includes(TextManager.LOADING_SPIN)) {
+			this._statusBar.text = text;
+			this._currentStatusBarText = text;
+			return;
+		}
+		if (!this._currentStatusBarText.includes(TextManager.LOADING_SPIN)) {
+			// This just now started the animation, set an interval
+			if (this._statusBarInterval) {
+				clearInterval(this._statusBarInterval);
+			}
+			this._statusBarInterval = setInterval(() => {
+				this._pushStatusBarText();
+			}, 1000);
+			this._statusBar.text = text;
+			this._currentStatusBarText = text;
+			return;
+		}
+
+		// Queue this new text
+		this._pendingStatusBarText = text;
+	}
+
+	public hide(): void {
+		this._statusBar.hide();
+		if (this._statusBarInterval) {
+			clearInterval(this._statusBarInterval);
+			this._statusBarInterval = null;
+		}
+	}
+
+	public show(): void {
+		this._statusBar.show();
+	}
+
+	public dispose(): void {
+		this.hide();
+		this._statusBar.dispose();
 	}
 }
