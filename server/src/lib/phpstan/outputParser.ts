5fd12a145189fd1ea27926d42349fc7796d033a7
Sander Ronde <sanderronde@crisp.nl>
2024-03-23 21:45:30 +0100

    Errors in traits are reported inside that trait and in the correct file (fixes #65)

--- server/src/lib/phpstan/outputParser.ts
+++ server/src/lib/phpstan/outputParser.ts
@@ -1,18 +1,31 @@
 import type { PHPStanCheckResult } from './runner';
 
+const IN_CONTEXT_OF_PREFIX = ' (in context of';
 export class OutputParser {
 	public constructor(private readonly _output: PHPStanCheckResult) {}
 
 	public parse(): ReportedErrors {
 		const notFileSpecificErrors: string[] = this._output.errors;
 		const fileSpecificErrors: ReportedErrors['fileSpecificErrors'] = {};
-		for (const filePath in this._output.files) {
-			fileSpecificErrors[filePath] = this._output.files[
-				filePath
-			].messages.map((message) => ({
-				message: message.message,
-				lineNumber: message.line,
-			}));
+		for (const key in this._output.files) {
+			let filePath = key;
+			let messagePostfix = '';
+
+			// PHPStan reports error in the context of traits as errors in separate
+			// files postfixed with some string. We remove this postfix and join
+			// the errors with the original file, moving the postfix to the message instead.
+			const contextIndex = key.indexOf(IN_CONTEXT_OF_PREFIX);
+			if (contextIndex !== -1) {
+				filePath = key.split(IN_CONTEXT_OF_PREFIX)[0];
+				messagePostfix = key.slice(contextIndex);
+			}
+			fileSpecificErrors[filePath] = [
+				...(fileSpecificErrors[filePath] ?? []),
+				...this._output.files[key].messages.map((message) => ({
+					message: message.message + messagePostfix,
+					lineNumber: message.line,
+				})),
+			];
 		}
 
 		return {
