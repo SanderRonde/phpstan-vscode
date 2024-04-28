22b9a9650a33522491d09b4ee16af2f389fc6ed3
Sander Ronde <awsdfgvhbjn@gmail.com>
2024-04-28 21:57:24 +0200

    Only kill zombie processes when the PID still refers to a PHPStan process

--- client/src/notificationReceivers/procSpawner.ts
+++ client/src/notificationReceivers/procSpawner.ts
@@ -2,7 +2,12 @@ import { processNotification } from '../lib/notificationChannels';
 import type { LanguageClient } from 'vscode-languageclient/node';
 import type { Disposable, ExtensionContext } from 'vscode';
 import { PROCESS_SPAWNER_PREFIX, log } from '../lib/log';
-import { wait } from '../../../shared/util';
+import { lookup, kill } from 'ps-node';
+
+interface ProcessDescriptor {
+	timeout: number;
+	binStr: string;
+}
 
 export class ProcessSpawner implements Disposable {
 	private static STORAGE_KEY = 'phpstan.processes';
@@ -22,7 +27,14 @@ export class ProcessSpawner implements Disposable {
 					'with timeout',
 					String(timeout)
 				);
-				void this._pushPid(pid, timeout);
+				lookup({ pid }, (err, list) => {
+					if (err || !list.length) {
+						void this._pushPid(pid, timeout);
+						return;
+					}
+
+					void this._pushPid(pid, timeout, list[0].command);
+				});
 			})
 		);
 		const interval = setInterval(() => this._kill(), 1000 * 60 * 30);
@@ -40,40 +52,56 @@ export class ProcessSpawner implements Disposable {
 		}
 	}
 
-	private async _killProc(pid: number): Promise<void> {
+	private async _killProc(pid: number, binStr?: string): Promise<void> {
 		if (!this._procExists(pid)) {
 			return;
 		}
-		try {
-			process.kill(pid, 'SIGINT');
-			// eslint-disable-next-line no-empty
-		} catch (e) {}
-		await wait(5000);
-		try {
-			process.kill(pid, 'SIGKILL');
-			// eslint-disable-next-line no-empty
-		} catch (e) {}
+		lookup({ pid }, (err, list) => {
+			if (err || list.length === 0) {
+				// No longer exists or something went wrong
+				return;
+			}
+
+			list.forEach(async (proc) => {
+				if (binStr && proc.command !== binStr) {
+					return;
+				}
+
+				kill(proc.pid, 'SIGINT', (err) => {
+					if (err) {
+						kill(proc.pid, 'SIGKILL');
+					}
+				});
+			});
+		});
 	}
 
 	private _kill(killTimeoutless: boolean = false): void {
 		const processes = this._context.workspaceState.get(
 			ProcessSpawner.STORAGE_KEY,
 			{}
-		) as Record<number, number>;
+		) as Record<number, number | ProcessDescriptor>;
 		if (Object.keys(processes).length === 0) {
 			return;
 		}
 
 		const killed: number[] = [];
-		Object.entries(processes).forEach(([pid, timeout]) => {
-			if (Date.now() > timeout && (timeout !== 0 || killTimeoutless)) {
+		Object.entries(processes).forEach(([pid, data]) => {
+			const descriptor =
+				typeof data === 'number'
+					? { timeout: data, binStr: undefined }
+					: data;
+			if (
+				Date.now() > descriptor.timeout &&
+				(descriptor.timeout !== 0 || killTimeoutless)
+			) {
 				const pidNum = parseInt(pid, 10);
 				killed.push(pidNum);
-				void this._killProc(pidNum);
+				void this._killProc(pidNum, descriptor.binStr);
 			}
 		});
 
-		const newProcesses: Record<number, number> = {};
+		const newProcesses: Record<number, number | ProcessDescriptor> = {};
 		for (const pid in processes) {
 			if (killed.includes(parseInt(pid, 10))) {
 				continue;
@@ -86,10 +114,20 @@ export class ProcessSpawner implements Disposable {
 		);
 	}
 
-	private async _pushPid(pid: number, timeout: number): Promise<void> {
+	private async _pushPid(
+		pid: number,
+		timeout: number,
+		binStr?: string
+	): Promise<void> {
+		const targetTime = timeout === 0 ? 0 : Date.now() + timeout;
 		await this._context.workspaceState.update(ProcessSpawner.STORAGE_KEY, {
 			...this._context.workspaceState.get(ProcessSpawner.STORAGE_KEY, {}),
-			[pid]: timeout === 0 ? 0 : Date.now() + timeout,
+			[pid]: binStr
+				? {
+						timeout: targetTime,
+						binStr,
+					}
+				: targetTime,
 		});
 	}
 
