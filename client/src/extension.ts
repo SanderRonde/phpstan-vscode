eeeb1e33784f54ad7f9311e7993ecb6cc99efd0f
Sander Ronde <awsdfgvhbjn@gmail.com>
2024-08-21 23:57:25 +0200

    Don't disable extension when enabled is set to false (still allows for manual checking)

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -80,10 +80,6 @@ async function startLanguageServer(
 }
 
 export async function activate(context: ExtensionContext): Promise<void> {
-	if (!getEditorConfiguration().get('phpstan.enabled')) {
-		return;
-	}
-
 	log(CLIENT_PREFIX, 'Initializing PHPStan extension');
 	createOutputChannel();
 	const client = await startLanguageServer(context);
