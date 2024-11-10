af763c38f3905666f54cb3a74f93b907c0807355
Sander Ronde <sanderronde@crisp.nl>
2024-11-10 18:09:57 +0100

    Add some demo/test files

---
+++ test/multi-config-demo/test/php/DemoClass.php
@@ -0,0 +1,26 @@
+<?php
+
+class X {
+	public string $y;
+}
+
+class DemoClass {
+	/**
+	 * @param 'a'|'b'|'c'|'d'|'e'|'f' $initialStr
+	 */
+	public function strUnion(string $initialStr) {
+	
+		
+		
+		if ($initialStr === 'a') {
+			return 0;
+		}
+		if ($initialStr === 'b' || $initialStr === 'c') {
+			return $initialStr;
+		}
+		if ($initialStr === 'd') {
+			return 1;
+		}
+		return $initialStr;
+	}
+}
