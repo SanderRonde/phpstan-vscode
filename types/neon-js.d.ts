7c38588573ab6c3c931451815492550d8b66b756
Sander Ronde <sanderronde@crisp.nl>
2024-11-10 19:45:26 +0100

    Add neon-js

---
+++ types/neon-js.d.ts
@@ -0,0 +1,15 @@
+declare module 'neon-js' {
+	export class Map {
+		public get(key: string): Neon;
+		public has(key: string): boolean;
+		public forEach(callback: (key: string, value: Neon) => void): void;
+		public isList(): boolean;
+		public values(): Neon[];
+		public keys(): string[];
+		public items(): { key: string; value: Neon }[];
+		public toObject(): Record<string, Neon>;
+	}
+	type Neon = string | number | boolean | Map;
+	export function decode(content: string): Neon;
+	export function encode(content: Neon): string;
+}
