3ebfd788843a813d139ff8106a576a2f19fa89f6
Sander Ronde <sanderronde@crisp.nl>
2025-01-12 14:51:43 +0100

    Remove (for now) non-existant rule

--- knip.ts
+++ knip.ts
@@ -3,7 +3,6 @@ import type { KnipConfig } from 'knip';
 const config: KnipConfig = {
 	entry: ['client/src/extension.ts', 'server/src/server.ts'],
 	project: ['{client,server}/**/*.{js,ts}'],
-	include: ['typeMembers'],
 	exclude: ['devDependencies', 'unlisted', 'binaries', 'unresolved'],
 };
 
